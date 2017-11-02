22bb293c8f29097637f1a74add0205d69750fc06
Widholm Kevin <Kevin.Widholm@novatec-gmbh.de>
2017-11-02 16:03:23 +0100

    first commit

--- train/DNNTrainingTest.py
+++ train/DNNTrainingTest.py
@@ -6,6 +6,7 @@ import argparse
 from nltk.tokenize import word_tokenize
 import nltk
 nltk.download('punkt')
+nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 import io
 import csv
