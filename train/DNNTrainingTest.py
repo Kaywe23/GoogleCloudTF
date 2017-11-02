da3cfb2187b93a85c669005cea412b189c9e5166
Widholm Kevin <Kevin.Widholm@novatec-gmbh.de>
2017-11-02 15:03:56 +0100

    first commit

--- train/DNNTrainingTest.py
+++ train/DNNTrainingTest.py
@@ -77,7 +77,8 @@ def train_neural_network(train_file='lexikon.pickle',csv_file='train_converted_v
         while epoch <= hm_epochs:
 
             epoch_loss = 1
-            gcs_file = pd.read_csv(tf.gfile.Open(csv_file, 'rb'),encoding='latin-1', buffer_lines=20000)
+            gcs_file = pd.read_csv(tf.gfile.Open(csv_file, 'rb'),encoding='latin-1', buffer_lines=20000,skipinitialspace=True)
+            gcs_file.to_csv()
             #csv_reader=gcs_file.read()
             #with io.open(csv_file1, buffering=20000, encoding='latin-1') as f:
             #df_train = pd.read_csv(tf.gfile.Open("./train.csv"),skipinitialspace=True)
