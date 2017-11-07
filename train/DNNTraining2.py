94422967a21d21696788039ca21d9a0ed27c1107
Widholm Kevin <Kevin.Widholm@novatec-gmbh.de>
2017-11-07 10:10:15 +0100

    first commit

--- train/DNNTraining2.py
+++ train/DNNTraining2.py
@@ -26,6 +26,7 @@ n_classes = 2
 batch_size = 100
 hm_epochs = 2
 datenanzahl = 100
+display_step = 1
 
 
 
@@ -99,7 +100,6 @@ def trainDNN(train_file='lexikon2.pickle',csv_file='train_converted_vermischt.cs
 
             saver.restore(sess,checkpoint)
             avg_cost=0
-            epoch_loss=1
             batch_count=int(datenanzahl)
 
             with tf.gfile.Open(csv_file, 'rb') as gcs_file:
@@ -125,9 +125,9 @@ def trainDNN(train_file='lexikon2.pickle',csv_file='train_converted_vermischt.cs
                         print('Es wurden', datenanzahl, 'Daten verarbeitet')
                         break
 
-                    _, c, summary = sess.run([optimizer, cost, summary_op],
+                    _,summary = sess.run([optimizer, summary_op],
                                              feed_dict={x: np.array(batch_x), y: np.array(batch_y)})
-                    epoch_loss += c
+
 
                     for _i in range(batch_count):
                         avg_cost += sess.run(cost, feed_dict={x: np.array(batch_x), y: np.array(batch_y)}) / batch_count
@@ -135,7 +135,7 @@ def trainDNN(train_file='lexikon2.pickle',csv_file='train_converted_vermischt.cs
 
             if epoch % display_step == 0:
 
-                print "Epoche:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost), "Loss=", "{:.9f}".format(epoch_loss)
+                print "Epoch:", '%04d' % (epoch+1),"of",'%04d' % (hm_epochs), "cost=", "{:.9f}".format(avg_cost)
 
             saver.save(sess, checkpoint)
             epoch += 1
