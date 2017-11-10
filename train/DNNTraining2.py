fe12843b2a1c1a1d7572d68bd77dff3b551ce2aa
Widholm Kevin <Kevin.Widholm@novatec-gmbh.de>
2017-11-11 00:02:57 +0100

    first commit

--- train/DNNTraining2.py
+++ train/DNNTraining2.py
@@ -26,8 +26,8 @@ n_nodes_hl3 = 1500
 
 n_classes = 2
 batch_size = 100
-hm_epochs = 2
-datenanzahl = 100
+hm_epochs = 15
+datenanzahl = 500000
 display_step = 1
 
 x = tf.placeholder('float')
@@ -94,7 +94,7 @@ def trainDNN(train_file='lexikon2.pickle',csv_file='train_converted_vermischt.cs
     cost_summary = tf.summary.scalar("cost", cost)
     acc_summary = tf.summary.scalar("accuracy", accuracy)
 
-    #summary_op = tf.summary.merge_all()
+    summary_op = tf.summary.merge_all()
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
@@ -131,10 +131,10 @@ def trainDNN(train_file='lexikon2.pickle',csv_file='train_converted_vermischt.cs
                     batch_x = np.array([list(features)])
                     batch_y = np.array([eval(label)])
 
-                    _, c, train_acc, _cost_summary, _acc_summary = sess.run([optimizer, cost, accuracy, cost_summary, acc_summary],
+                    _, c, summary = sess.run([optimizer, cost, summary_op],
                                              feed_dict={x: np.array(batch_x), y: np.array(batch_y)})
-                    writer.add_summary(_cost_summary, epoch * datenanzahl + zaehler)
-                    writer.add_summary(_acc_summary, epoch * datenanzahl + zaehler)
+                    writer.add_summary(summary, epoch * datenanzahl + zaehler)
+
                     avg_cost += c / datenanzahl
 
                     if zaehler > datenanzahl:
@@ -143,8 +143,8 @@ def trainDNN(train_file='lexikon2.pickle',csv_file='train_converted_vermischt.cs
             saver.save(sess, checkpoint)
             if epoch % display_step == 0:
 
-                print "Epoch:", '%04d' % (epoch+1),"of",'%04d' % (hm_epochs), "cost=", "{:.9f}".format(avg_cost),
-                "train_acc=", "{:.9f}".format(train_acc)
+                print "Epoch:", '%04d' % (epoch+1),"of",'%04d' % (hm_epochs), "cost=", "{:.9f}".format(avg_cost)
+
 
 
 
